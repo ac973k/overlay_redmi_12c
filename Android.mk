c520963c65a2e50dd7a229cc5d2d3ab9abb7a6a8
ac973k <daniil@terentyeff.ru>
2024-03-18 09:25:22 +0300

    bug fix

--- Android.mk
+++ Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_PACKAGE_NAME := treble-overlay-xiaomi-redmi_12c
+LOCAL_PACKAGE_NAME := treble-overlay-xiaomi-redmi12c
 LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/overlay
 LOCAL_IS_RUNTIME_RESOURCE_OVERLAY := true
 LOCAL_PRIVATE_PLATFORM_APIS := true
